d5554822a5c6ce88e7a2228296eaf37ba84ab49b
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2021-05-14 16:40:15 +0200

    :art: Hotfixes

--- configs/cors.config.js
+++ configs/cors.config.js
@@ -1,11 +1,12 @@
 const cors = require("cors");
-const { FRONTEND_ENDPOINT } = process.env;
+const { FRONTEND_ENDPOINT_DEVELOPMENT, FRONTEND_ENDPOINT_PRODUCTION } =
+  process.env;
 
 module.exports = (incomingApp) => {
   incomingApp.use(
     cors({
       credentials: true,
-      origin: FRONTEND_ENDPOINT,
+      origin: [FRONTEND_ENDPOINT_DEVELOPMENT, FRONTEND_ENDPOINT_PRODUCTION],
     })
   );
 };
