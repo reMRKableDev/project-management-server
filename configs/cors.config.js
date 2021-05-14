4fff65273b5e5fd4d17df287f06e7ecf6e0e3186
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2021-05-14 18:24:44 +0200

    :poop: console log placed temporarily

--- configs/cors.config.js
+++ configs/cors.config.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const { FRONTEND_ENDPOINT } = process.env;
 
 module.exports = (incomingApp) => {
+  console.log("FE", FRONTEND_ENDPOINT);
   incomingApp.use(
     cors({
       credentials: true,
