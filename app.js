1d32367ff8c3797858a3d914f6a169fb9e9e4821
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2021-05-14 11:34:14 +0200

    :fire: Clean up of unused files and folders

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ const path = require("path");
 const cors = require("cors");
 const session = require("express-session");
 const passport = require("passport");
-const MongoStore = require("connect-mongo")(session);
 
+// CONNECT DATABASE CONFIGS
 require("./configs/db.config");
-require("./configs/passport");
+
+// CONNECT PASSPORT CONFIGS
+require("./configs/passport.config");
 
 const app_name = require("./package.json").name;
 const debug = require("debug")(
@@ -29,15 +31,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
-app.use(
-  require("node-sass-middleware")({
-    src: path.join(__dirname, "public"),
-    dest: path.join(__dirname, "public"),
-    sourceMap: true,
-  })
-);
-
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -66,8 +59,6 @@ app.use(
 );
 
 // ROUTES MIDDLEWARE STARTS HERE:
-
-app.use("/", require("./routes/index"));
 app.use("/api", require("./routes/project-routes"));
 app.use("/api", require("./routes/task-routes"));
 app.use("/api", require("./routes/auth-routes"));
