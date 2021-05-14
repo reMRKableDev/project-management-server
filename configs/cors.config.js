82fecf8ed0f1743633f5f1747d2c75311ad08381
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2021-05-14 16:21:46 +0200

    :wrench: Changed the env for cors endpoint

--- configs/cors.config.js
+++ configs/cors.config.js
@@ -1,11 +1,11 @@
 const cors = require("cors");
-const { CORS_ORIGIN } = process.env;
+const { FRONTEND_ENDPOINT } = process.env;
 
 module.exports = (incomingApp) => {
   incomingApp.use(
     cors({
       credentials: true,
-      origin: CORS_ORIGIN,
+      origin: FRONTEND_ENDPOINT,
     })
   );
 };
